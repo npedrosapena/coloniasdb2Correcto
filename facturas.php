74608069d97f3bc846cac4b88cc5b7edc88607da
nelson <npedrosapena@danielcastelao.org>
2015-02-17 14:53:10 +0100

    agregado archivo facturas.php

---
+++ facturas.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Colonias Canguesas. Informaci&oacute;n interna</title>
+        <link type="text/css" rel="stylesheet" href="css/index.css" />
+    </head>
+    <body>
+        <div id="wrapper">
+            
+            
+        </div>
+    </body>
+</html>
+
+
